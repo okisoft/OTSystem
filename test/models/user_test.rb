fc61949f71acebca13b9e5fb852cc227ea9fd248
nnks1010 <ntomoya1010@gmail.com>
2018-01-03 17:29:44 +0900

    add user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(user_id: "user", name: "example user", authority: 1,
+                     password: "hogehoge", password_confirmation: "hogehoge")
+  end
+
+  test "有効であること" do
+    assert @user.valid?
+  end
+
+  test "user_id がなければ無効であること" do
+    @user.user_id = ""
+    assert_not @user.valid?
+  end
+
+  test "name がなければ無効であること" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "authority がなければ無効であること" do
+    @user.authority = ""
+    assert_not @user.valid?
+  end
+
+  test "user_id が長ければ無効であること" do
+    @user.user_id = "a" * 256
+    assert_not @user.valid?
+  end
+
+  test "name が長ければ無効であること" do
+    @user.name = "a" * 256
+    assert_not @user.valid?
+  end
+
+  test "authority が範囲外であれば無効であること" do
+    @user.authority = 4
+    assert_not @user.valid?
+    @user.authority = 0
+    assert_not @user.valid?
+  end
+
+  test "user_id が複数あれば無効であること" do
+    dup_user = @user.dup
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "password が空文字であれば無効であること" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password が短ければ無効であること" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
