3557c61c4737dfc7c71a535e7e0d3544ad144e69
nnks1010 <ntomoya1010@gmail.com>
2018-01-03 23:40:09 +0900

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "OTSystem"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
