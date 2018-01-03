7b9d1adc14edc893e274e4958354f6072e852d73
nnks1010 <ntomoya1010@gmail.com>
2018-01-04 01:56:33 +0900

    add user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.authority = 1
+    if @user.save
+      flash[:success] = "登録しました"
+      # redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:user_id, :name, :password, :password_confirmation)
+    end
+end
