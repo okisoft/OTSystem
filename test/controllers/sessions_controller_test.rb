e1c46d625cec8f038ed584378e7465cd278f6a2c
nnks1010 <ntomoya1010@gmail.com>
2018-01-06 02:48:00 +0900

    test failure

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,24 +7,31 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "ユーザがログインしている場合はホーム画面を表示すること" do
+    assert false
   end
 
   test "管理者がログインしている場合は管理画面を表示すること" do
+    assert false
   end
 
   test "セッションがない場合はログインページを表示すること" do
+    assert false
   end
 
   test "ユーザがログインできること" do
+    assert false
   end
 
   test "ユーザがログアウトできること" do
+    assert false
   end
 
   test "無効なユーザIDの場合はログインできないこと" do
+    assert false
   end
 
   test "無効なパスワードの場合はログインできないこと" do
+    assert false
   end
 
 end
