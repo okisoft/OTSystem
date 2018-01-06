a11300c2aa101e1539d683ec031ee7fea25eafd2
nnks1010 <ntomoya1010@gmail.com>
2018-01-06 09:59:46 +0900

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root  to: 'static_pages#home'
+  get     '/home',    to: 'static_pages#home'
+  get     '/admin',   to: 'static_pages#admin'
   get     '/signup',  to: 'users#new'
   post    '/signup',  to: 'users#create'
   get     '/login',   to: 'sessions#new'
