36a15711dac4e0d0e334e6beec5a401d9796f7bb
hisakawa <hirurupm@ezweb.ne.jp>
2018-01-14 15:24:00 +0900

    problemとquestionについてのテストを作成しました

--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    lecture = Lecture.new(name: "software")
+    lecture.save
+    lecture_year = LectureYear.new(lecture_id: lecture.id, year: 2018, style: "個人")
+    lecture_year.save
+    lecture_time = LectureTime.new(lecture_year_id: lecture_year.id, time: 1, title: "ソフ工")
+    lecture_time.save
+    @problem = Problem.new(lecture_time_id: lecture_time.id, name: "レビュ表", content: "レビュを書く")
+  end
+
+  test "有効であること" do
+    assert @problem.valid?
+  end
+
+  test "lecture_time_id がなければ無効であること" do
+    @problem.lecture_time_id = ""
+    assert_not @problem.valid?
+  end
+
+  test "name がなければ無効であること" do
+    @problem.name = ""
+    assert_not @problem.valid?
+  end
+
+  test "content がなければ無効であること" do
+    @problem.content = ""
+    assert_not @problem.valid?
+  end
+
+  test "name が長ければ無効であること" do
+    @problem.name = "a" * 33
+    assert_not @problem.valid?
+  end
+
+  test "content が長ければ無効であること" do
+    @problem.content = "a" * 513
+    assert_not @problem.valid?
+  end
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    lecture = Lecture.new(name: "software")
+    lecture.save
+    lecture_year = LectureYear.new(lecture_id: lecture.id, year: 2018, style: "個人")
+    lecture_year.save
+    lecture_time = LectureTime.new(lecture_year_id: lecture_year.id, time: 1, title: "ソフ工")
+    lecture_time.save
+    problem = Problem.new(lecture_time_id: lecture_time.id, name: "レビュ表", content: "レビュを書く")
+    problem.save
+    @question = Question.new(problem_id: problem.id, user_name: "nonaka", group_name: "OTsystem",
+                              content: "わからん", reply: "わからんわかる", visible: 1)
+  end
+
+  test "有効であること" do
+    assert @question.valid?
+  end
+
+  test "content がなければ無効であること" do
+    @question.content = ""
+    assert_not @question.valid?
+  end
+
+  test "visible がなければ無効であること" do
+    @question.visible = ""
+    assert_not @question.valid?
+  end
+
+  test "user_name が長ければ無効であること" do
+    @question.user_name = "a" * 17
+    assert_not @question.valid?
+  end
+
+  test "group_name が長ければ無効であること" do
+    @question.group_name = "a" * 17
+    assert_not @question.valid?
+  end
+
+  test "content が長ければ無効であること" do
+    @question.content = "a" * 513
+    assert_not @question.valid?
+  end
+
+  test "reply が長ければ無効であること" do
+    @question.reply = "a" * 513
+    assert_not @question.valid?
+  end
 end
