bcf4d73f97128110f8c7219013e95790c6c74ebd
To07 <TakashiOKI@DESKTOP-JV4R4KU.localdomain>
2018-01-17 23:08:38 +0900

    generated question-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root  to: 'static_pages#assign'
-  get     '/home',    to: 'static_pages#home'
-  get     '/admin',   to: 'static_pages#admin'
-  get     '/signup',  to: 'users#new'
-  post    '/signup',  to: 'users#create'
-  get     '/login',   to: 'sessions#new'
-  post    '/login',   to: 'sessions#create'
-  delete  '/logout',  to: 'sessions#destroy'
+  get     '/home',       to: 'static_pages#home'
+  get     '/admin',      to: 'static_pages#admin'
+  get     '/signup',     to: 'users#new'
+  post    '/signup',     to: 'users#create'
+  get     '/login',      to: 'sessions#new'
+  post    '/login',      to: 'sessions#create'
+  delete  '/logout',     to: 'sessions#destroy'
+  get     '/quiestions', to: 'questions#index'
   resources :users, only: [:create, :new, :edit, :update]
   resources :students, only: [:create, :new]
   resources :lectures, only: [:create, :new] do
