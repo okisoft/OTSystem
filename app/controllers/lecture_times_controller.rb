5cb7d25ba670e0afee412fdf3b3e4d87c9511794
hisakawa <hirurupm@ezweb.ne.jp>
2018-01-21 02:11:02 +0900

    一旦

--- app/controllers/lecture_times_controller.rb
+++ app/controllers/lecture_times_controller.rb
@@ -1,7 +1,15 @@
 class LectureTimesController < ApplicationController
   @problems = []  # 初期化
+  @progresses = []
   def questions_index
     @lecture_time = LectureTime.find(params[:id])
+    #binding.pry
     @problems = @lecture_time.problems.all
+  end
+
+  def progresses_index
+    @lecture_time = LectureTime.find(params[:id])
+    binding.pry
+    @progresses = @lecture_time.progresses.all
   end
 end
