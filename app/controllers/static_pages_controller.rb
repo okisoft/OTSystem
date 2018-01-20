368ac224a30465f78886efe7cda6622c5236f072
To07 <TakashiOKI@DESKTOP-JV4R4KU.localdomain>
2018-01-20 16:33:26 +0900

    questions-index in home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: [:assign]
+  @problems = []  # 初期化
 
   def assign
     if !logged_in?
@@ -19,6 +20,8 @@ class StaticPagesController < ApplicationController
       if public_lecture.lecture_time_id.nil?
         render_not_open
       else
+        @public_lecture_time = Lecture.find_by(id: public_lecture.lecture_id).lecture_years.last.lecture_times.find_by(id: public_lecture.lecture_time_id)
+        #problems = @public_lecture_time.problems.all
         lecture_year = public_lecture.lecture_time.lecture_year
         if current_user.lecture_years.find_by(id: lecture_year.id).nil?
           redirect_to new_student_path
