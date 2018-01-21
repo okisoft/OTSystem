1f655633bc9d3f5d485ae8524456c6df7fc8f1a3
hisakawa <hirurupm@ezweb.ne.jp>
2018-01-21 16:36:55 +0900

    achievments の表示を作成

--- app/controllers/lecture_times_controller.rb
+++ app/controllers/lecture_times_controller.rb
@@ -6,8 +6,14 @@ class LectureTimesController < ApplicationController
 
   def progresses_index
     lecture_time = LectureTime.find(params[:id])
-    @progresses = lecture_time.progresses.all
-    #binding.pry
-    #@ahievments = @lecture_time.problems
+    progresses = lecture_time.progresses.all
+
+    @achievments_array = []
+    progress_count = Array.new(lecture_time.problems.count, 0) # 課題のレコード数を取得
+    progresses.each do |progress|
+      @achievments_array.push([progress, progress_count]) # 課題が3つなら[nil, nil, nil]を配列に追加
+    end
+    # userの数:2、課題の数:3なら
+    # [[nil,nil,nil],[nil,nil,nil]]
   end
 end
