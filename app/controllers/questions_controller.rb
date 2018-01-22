0bb481a8b857a20246ed7dc43ec960bf1b0fb736
To07 <TakashiOKI@DESKTOP-JV4R4KU.localdomain>
2018-01-22 22:41:53 +0900

    create reply-system

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,6 +20,20 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      @lecture_time = @question.problem.lecture_time
+      redirect_to questions_lecture_time_path(@lecture_time.id)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def question_params
