45f141b114196d6cc18f02bfb2da7f1b02f771bc
To07 <TakashiOKI@DESKTOP-JV4R4KU.localdomain>
2018-01-22 22:57:38 +0900

    fixed bug

--- app/controllers/lecture_times_controller.rb
+++ app/controllers/lecture_times_controller.rb
@@ -5,10 +5,10 @@ class LectureTimesController < ApplicationController
   end
 
   def progresses_index
-    lecture_time = LectureTime.find(params[:id])
-    progresses = lecture_time.progresses.all
+    @lecture_time = LectureTime.find(params[:id])
+    progresses = @lecture_time.progresses.all
     @achievments_array = []
-    progress_count = Array.new(lecture_time.problems.count, 0) # 課題のレコード数を取得
+    progress_count = Array.new(@lecture_time.problems.count, 0) # 課題のレコード数を取得
     progresses.each do |progress|
       @achievments_array.push([progress, progress_count]) # 課題が3つなら[nil, nil, nil]を配列に追加
     end
@@ -16,8 +16,8 @@ class LectureTimesController < ApplicationController
     # [[nil,nil,nil],[nil,nil,nil]]
 
     # public_lectureの更新
-    lecture = lecture_time.lecture_year.lecture
+    lecture = @lecture_time.lecture_year.lecture
     @public_lectures = PublicLecture.all
-    @public_lectures.update(user_id: current_user.id, lecture_id: lecture.id, lecture_time_id: lecture_time.id)
+    @public_lectures.update(user_id: current_user.id, lecture_id: lecture.id, lecture_time_id: @lecture_time.id)
   end
 end
