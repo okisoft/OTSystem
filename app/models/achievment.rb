d35965284f629b6e941a5dd2c17e288a81d2d580
hisakawa <hirurupm@ezweb.ne.jp>
2018-01-23 22:20:15 +0900

    added achieved in achievment-model

--- app/models/achievment.rb
+++ app/models/achievment.rb
@@ -1,4 +1,6 @@
 class Achievment < ApplicationRecord
   belongs_to :user
   belongs_to :problem
+  validate :achieved,         presence: true,
+                              inclusion: { in: [true, false] }
 end
